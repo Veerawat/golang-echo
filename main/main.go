4f5bf11dc93b027499c284fe421abaf4ad37613b
wat <veerawat74@gmail.com>
2018-07-08 19:16:50 +0700

    part3

--- main/main.go
+++ main/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
+
+type Cat struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
 
 func index(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
@@ -33,11 +41,32 @@ func getCats(c echo.Context) error {
 	})
 }
 
-func main() {
-	fmt.Println("hello world")
+func addCat(c echo.Context) error {
+	cat := Cat{}
 
+	defer c.Request().Body.Close()
+
+	b, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("Fail reading the reqest body for addCats: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &cat)
+	if err != nil {
+		log.Printf("Fail Unmarshaling in add Cats: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("this is your cat: %#v", cat)
+	return c.String(http.StatusOK, "we got your cat!")
+}
+
+func main() {
 	e := echo.New()
 	e.GET("/", index)
 	e.GET("/cat/:data", getCats)
-	e.Logger.Fatal(e.Start(":8080"))
+
+	e.POST("/cats", addCat)
+	e.Logger.Fatal(e.Start(":8000"))
 }
